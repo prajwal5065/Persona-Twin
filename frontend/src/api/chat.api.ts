f5753048c49ddaea1d401e0f29a555daae5bd86b
Prajwal Kamte <kamteprajwal@gmail.com>
2026-04-26 11:25:08 +0530

    Revert "feaa: added frontend (#20)" (#21)
    This reverts commit e8cd947b5a391f42443c46b3132756b5604d9147.

--- frontend/src/api/chat.api.ts
+++
@@ -1,5 +0,0 @@
-import client from './client';
-
-export const chatApi = {
-  sendMessage: (query: string) => client.post('/chat', { query }),
-};
--- frontend/src/api/profile.api.ts
+++
@@ -1,5 +0,0 @@
-import client from './client';
-
-export const profileApi = {
-  getPersonality: () => client.get('/profile/personality'),
-};
